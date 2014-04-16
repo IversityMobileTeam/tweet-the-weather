f788f110ed9719363f7b0f363385b08a70cb1138
bewitharindam <bewitharindam@gmail.com>
2014-04-16 20:59:05 +0530

    Implemented Tweet model #3

---
+++ app/model/Tweet.js
@@ -0,0 +1,12 @@
+Ext.define('TweetTheWeather.model.Tweet', {
+    extend: 'Ext.data.Model',
+    
+    config: {
+        fields: [
+            { name: 'text', type: 'string' },
+            { name: 'positive', type: 'boolean' },
+            { name: 'tags', type: 'auto' }
+
+        ]
+    }
+});
