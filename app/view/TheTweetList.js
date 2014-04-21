45b7d11b7907960e425550f90bc18e020b1e31d2
andrIvash <andr.ivas12@gmail.com>
2014-04-21 21:51:41 +0300

    Update TheTweetList.js

--- app/view/TheTweetList.js
+++ app/view/TheTweetList.js
@@ -1,52 +1,43 @@
-Ext.define("TweetTheWeather.view.TheTweetList", {
-    extend: "Ext.Container",
-    requires:"Ext.dataview.List",
-    alias: "widget.tweetview",
-
-    config: {
-        layout: {
+Ext.define('TweetTheWeather.view.TweetList', {
+    extend: 'Ext.Container',
+    requires:
+    [
+        'Ext.dataview.List',
+        'TweetTheWeather.store.Tweet',
+        'Ext.TitleBar',
+        'Ext.Button'
+    ],
+    alias: 'widget.tweetlist',
+    config: 
+    {
+        layout: 
+        {
             type: 'fit'
         },
-        items: [{
-            xtype: "toolbar",
-            title: "TweetList",
-            docked: "top",
-            items: [
-               
-                {
-                    xtype: "button",
-                    text: 'Back',
-                    ui: 'back',
-                    itemId: "backButton"
-                },
-                 { xtype: 'spacer' },
-            ]
-        }, {
-            xtype: "list",
-            store: "TweetStore",
-            itemId:"TweetList",
-            loadingText: "Loading Notes...",
-            emptyText: '<div class="tweet-list-empty-text">No tweets found.</div>',
-            onItemDisclosure: true,
-           
-            itemTpl: '<div class="list-tweet-text">{text}</div>'
-        }],
-        listeners: [{
-            delegate: "#backButton",
-            event: "tap",
-            fn: "onBackButtonTap"
-        }, {
-            delegate: "#TweetList",
-            event: "disclose",
-            fn: "onTweetListDisclose"
-        }]
-    },
-    onBackButtonTap: function () {
-        console.log("backTweetCommand");
-        this.fireEvent("backTweetCommand", this);
-    },
-    onTweetListDisclose: function (list, record, target, index, evt, options) {
-        console.log("moreTweetCommand");
-        this.fireEvent('moreTweetCommand', this, record);
-    }
+        items: 
+        [
+            {
+            xtype: 'titlebar',
+            title: 'Tweets',
+            docked: 'top',
+            items: 
+                [
+                    {
+                        xtype: 'button',
+                        text: 'Back',
+                        ui: 'back'
+                    }
+                ]
+            },
+            {
+                xtype: 'list',
+                store: 'TweetStore',
+                itemId: 'TweetList',
+                loadingText: 'Loading Tweets...',
+                emptyText: '<div class="tweet-list__empty">No tweets found.</div>',
+                onItemDisclosure: true,
+                itemTpl: '<div class="tweet-list__item">{text}</div>'
+            }
+        ]
+     }
 });
