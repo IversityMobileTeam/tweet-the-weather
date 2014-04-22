eb0381731f348e96e697673b1e83ae43341b703c
andrIvash <andr.ivas12@gmail.com>
2014-04-22 18:34:08 +0300

    Create Main.js

---
+++ app/controller/Main.js
@@ -0,0 +1,12 @@
+Ext.define("TweetTheWeather.controller.Main", {
+    extend: "Ext.app.Controller",
+    config: {
+        refs: {
+            
+        },
+        control: {
+           
+        }
+    }
+    
+});
