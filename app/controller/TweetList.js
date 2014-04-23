82e116e6859f02ceab7658636ae185de4739d440
jviotti <juanchiviotti@gmail.com>
2014-04-23 15:08:29 -0400

    Remove some white lines and add a dump placeholder on onBackButton until Main View is implemented #24

--- app/controller/TweetList.js
+++ app/controller/TweetList.js
@@ -1,31 +1,24 @@
 Ext.define('TweetTheWeather.controller.TweetList', {
-    
     extend: 'Ext.app.Controller',
-    
     requires: [
         'TweetTheWeather.view.TweetList'
     ],
-    
     config: {
         views: [
             'TweetList'
         ],
-        
         refs: {
             backButton: '#backButton'
         },
-        
         control: {
             backButton: {
                 tap: 'onBackButton'
             }
         }
     },
-    
+
     onBackButton: function(button, event, options) {
-        button.up('navigationview').push({
-            xtype: 'MainView',
-        });
-    },
+        // TODO
+    }
 
 });
