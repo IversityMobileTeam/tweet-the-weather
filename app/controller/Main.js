3d45d1c919c06b26ddb615aaaea7e897420e3bb9
jviotti <juanchiviotti@gmail.com>
2014-04-23 12:53:30 -0400

    Fix some spacing and indentation from Main Controller #13

--- app/controller/Main.js
+++ app/controller/Main.js
@@ -1,44 +1,37 @@
 Ext.define('TweetTheWeather.controller.Main', {
-
-extend: 'Ext.app.Controller',
-
-requires: [
-    'TweetTheWeather.view.TweetList'
-],
-
-config: {
-    views: [
-        'Main'
+    extend: 'Ext.app.Controller',
+    requires: [
+        'TweetTheWeather.view.TweetList'
     ],
-
-    refs: {
-        likeButton: 'likeButton',
-        dislikeButton: 'dislikeButton'
-        
+    config: {
+        views: [
+            'Main'
+        ],
+        refs: {
+            likeButton: 'likeButton',
+            dislikeButton: 'dislikeButton'
+        },
+        control: {
+            '#likeButton': {
+                tap: 'onLikeButton'
+            },
+            '#dislikeButton': {
+                tap: 'onDislikeButton'
+            }
+        }
     },
 
-    control: {
-        '#likeButton': {
-            tap: 'onLikeButton'
-        },
-        '#dislikeButton': {
-            tap: 'onDislikeButton'
-        }
+    onLikeButton: function(button, event, options) {
+        button.up('navigationview').push({
+            xtype: 'tweetlist',
+            title: 'Positive Tweet'
+        });
+    },
+
+    onDislikeButton: function(button, event, options) {
+        button.up('navigationview').push({
+            xtype: 'tweetlist',
+            title: 'Negative Tweet'
+        });
     }
-},
-
-onLikeButton: function(button, event, options) {
-    button.up('navigationview').push({
-        xtype: 'tweetlist',
-        title: 'Positive Tweet'
-    });
-},
-
-onDislikeButton: function(button, event, options) {
-    button.up('navigationview').push({
-        xtype: 'tweetlist',
-        title: 'Negative Tweet'
-    });
-}
-
 });
