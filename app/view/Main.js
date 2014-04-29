cac574bc5f4724b355b0f786d62af20fc8242df3
andrIvash <andr.ivas12@gmail.com>
2014-04-29 22:37:29 +0300

    Update Main.js

--- app/view/Main.js
+++ app/view/Main.js
@@ -48,17 +48,31 @@ Ext.define('TweetTheWeather.view.Main', {
             }
         ]
     },
-    initialize: function(me, eOpts) {
-        var long = 48.834;
-        var lat = 2.394;
-        Ext.data.JsonP.request(
-            { 
-                url: 'http://api.worldweatheronline.com/free/v1/weather.ashx?q='+long+'%2C'+lat+'&format=json&num_of_days=5&key=tsd3famkb4qeau8d9fx7gmjt',
-                method:'GET',
-                success: function(result, request) {
-                    console.log(result.data.current_condition[0].temp_C);
+    initialize: function(){
+        Geolocation.getCurrentCoordinates(function(err, coords) {
+                if(err && err.message === 'User denied Geolocation') {
+                    return alert('You have to allow Geolocation!')
                 }
-            }
-       );
+            
+            Ext.data.JsonP.request(
+                { 
+                    url: 'http://api.worldweatheronline.com/free/v1/weather.ashx?q='+coords.latitude+'%2C'+coords.longitude+'&format=json&num_of_days=5&key=tsd3famkb4qeau8d9fx7gmjt',
+                    method:'GET',
+                    success: function(result, request) {
+                        console.log(result.data.current_condition[0].temp_C);
+                    }
+                }
+            );
+            
+            Ext.data.JsonP.request(
+                { 
+                    url: 'http://api.worldweatheronline.com/free/v1/search.ashx?key=tsd3famkb4qeau8d9fx7gmjt&query='+coords.latitude+'%2C'+coords.longitude+'&num_of_results=1&format=json',
+                    method:'GET',
+                    success: function(cityname, request) {
+                        console.log(cityname.search_api.result[0].areaName[0].value);
+                    }
+                }
+            );
+        });
     }
 });
