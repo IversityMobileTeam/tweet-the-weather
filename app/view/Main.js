f25694ab39db070c5aec09b0a7b227e2f4245faf
andrIvash <andr.ivas12@gmail.com>
2014-05-12 23:06:57 +0300

    Update Main.js

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,61 +1,58 @@
  Ext.define('TweetTheWeather.view.Main', {
-     extend: 'Ext.NavigationView', 
-      xtype: 'main',
-      requires: [
-          'Ext.TitleBar',
-		   ],
-      config: {       
-  
-          items: [
- 
-             
- 	     {
- 		title: 'Tweet the Weather'
- 	     },
- 	     {
- 		layout: {
-		type: 'vbox',
-		cls: 'my-class',
-		align: 'center'
-			},
+	extend: 'Ext.NavigationView', 
+    xtype: 'main',
+    requires: [
+     	'Ext.TitleBar',
+    ],
+    config: {       
 		items: [
-			    {
- 				xtype: 'label' ,
-				html: '<p class = "small"> Buenos Aires, Argentina</p>',
+			{
+	      		title: 'Tweet the Weather'
+ 	   		},
+ 	   		{
+ 	      		layout: {
+ 	      			type: 'vbox',
+					cls: 'my-class',
+					align: 'center'
+	      		},
+	      		items: [
+	         		{
+ 						xtype: 'label' ,
+						html: '<p class ="small"> Buenos Aires, Argentina</p>'
 		            },
- 			    {
-  				xtype: 'label' ,
- 				html: '<p class = "big"> <b>35° C </b> </p>',
- 			    },
- 			    {
-  				xtype: 'label' ,
- 				html: '<p class = "medium"><b>It&apos;s pretty hot!</b></p>',
- 			    },
- 			    {
-  				xtype: 'label' ,
- 				html: '<p class = "small"> Do you like it? <br><br> </p>',
+ 			    	{
+  						xtype: 'label' ,
+ 						html: '<p class ="big"> <b>35° C </b> </p>'
+ 			    	},
+ 			    	{
+  						xtype: 'label' ,
+ 						html: '<p class ="medium"><b>It&apos;s pretty hot!</b></p>'
+ 			    	},
+ 			    	{
+  						xtype: 'label' ,
+ 						html: '<p class ="small"> Do you like it? <br><br> </p>'
 		            },
-			    {
- 				layout: {
-				type: 'vbox',
-				align: 'center'
-					},
-					items: [
+			    	{
+ 						layout: {
+							type: 'vbox',
+							align: 'center'
+						},
+						items: [
  			    			{
-  						xtype: 'label' ,
-						left: '10px',
- 						html: '<img src="app/view/thumb-down.jpg"/width=146 height=166>',
-		            			},
+  								xtype: 'label' ,
+								left: '10px',
+ 								html: '<img src="app/view/thumb-down.jpg"/width=146 height=166>'
+		            		},
  			    			{
-  						xtype: 'label' ,
-						right: '10px',
- 						html: '<img src="app/view/thumb-up.jpg"/width=146 height=166>',
+  								xtype: 'label' ,
+								right: '10px',
+ 								html: '<img src="app/view/thumb-up.jpg"/width=146 height=166>'
 			    			},
 						]		
- 					     }
-			]		
- 	     } 
-          ]
+ 					}
+				]		
+ 	     	} 
+         ]
       }
   });
  
