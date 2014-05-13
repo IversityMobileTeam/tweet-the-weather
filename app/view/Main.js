7329519d95bb9551a038ce825547e625142d9af6
mytobygithub <tobi.bersch@gmx.de>
2014-05-14 00:06:14 +0200

    edited main view 5

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,51 +1,61 @@
-Ext.define('TweetTheWeather.view.Main', {
-    extend: 'Ext.tab.Panel',
-    alias: 'widget.MainView',
-    xtype: 'main',
-    requires: [
-        'Ext.TitleBar',
-        'Ext.Video'
-    ],
-    config: {
-        tabBarPosition: 'bottom',
-
-        items: [
-            {
-                title: 'Welcome',
-                iconCls: 'home',
-
-                styleHtmlContent: true,
-                scrollable: true,
-
-                items: {
-                    docked: 'top',
-                    xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
-                },
-
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
-            },
-            {
-                title: 'Get Started',
-                iconCls: 'action',
-
-                items: [
-                    {
-                        docked: 'top',
-                        xtype: 'titlebar',
-                        title: 'Getting Started'
-                    },
-                    {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
-                    }
-                ]
-            }
-        ]
-    }
-});
+ Ext.define('TweetTheWeather.view.Main', {
+     extend: 'Ext.NavigationView', 
+      xtype: 'main',
+      requires: [
+          'Ext.TitleBar',
+		   ],
+      	config: {       
+  
+          items: [             
+ 	     {
+ 		title: 'Tweet the Weather'
+ 	     },
+ 	     {
+ 	layout: {
+	type: 'vbox',
+	align: 'center'
+		},
+		items: [
+			    {
+ 				xtype: 'label',
+				cls: ['font--small'],
+				html: '<p> Buenos Aires, Argentina</p>',
+		            },
+ 			    {
+  				xtype: 'label',
+				cls: ['font--big','font--bold'],
+ 				html: '<p>35° C</p>',
+ 			    },
+ 			    {
+  				xtype: 'label',
+				cls: ['font--medium','font--bold'],
+ 				html: '<p>It&apos;s pretty hot!</p>',
+ 			    },
+ 			    {
+  				xtype: 'label',
+				cls: ['font--small'],
+ 				html: '<p> Do you like it?</p>',
+		            },
+			    {
+ 				layout: {
+				type: 'vbox',
+				align: 'center'
+					},
+					items: [
+ 			    			{
+  						xtype: 'label',
+						left: '10px',
+ 						html: '<img src="resources/images/thumb-down.jpg"/width=146 height=166>',
+		            			},
+ 			    			{
+  						xtype: 'label',
+						right: '10px',
+ 						html: '<img src="resources/images/thumb-up.jpg"/width=146 height=166>',
+			    			},
+						]		
+ 					     }
+			]		
+ 	    	 } 
+         	 ]
+     	 }
+  }); 
