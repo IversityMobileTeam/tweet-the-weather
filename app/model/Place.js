ff81f3f964415c20ac086ca6cc6bac0358aaafc9
andrIvash <andr.ivas12@gmail.com>
2014-05-13 20:42:10 +0300

    Create Place.js
    place model

---
+++ app/model/Place.js
@@ -0,0 +1,11 @@
+Ext.define('TweetTheWeather.model.Place', {
+    extend: 'Ext.data.Model',
+    
+    config: {
+        
+        fields: [
+            { name: 'areaName', type: 'string' },
+            { name: 'country', type: 'string' }
+        ]
+    }
+});
