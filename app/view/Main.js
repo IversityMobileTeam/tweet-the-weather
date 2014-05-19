000690af0fe4656a7547938d5d26008b7ee483d6
andrIvash <andr.ivas12@gmail.com>
2014-05-19 18:06:26 +0300

    Update Main.js

--- app/view/Main.js
+++ app/view/Main.js
@@ -8,21 +8,17 @@ Ext.define('TweetTheWeather.view.Main', {
     ],
     config: {
         tabBarPosition: 'bottom',
-
         items: [
             {
                 title: 'Welcome',
                 iconCls: 'home',
-
                 styleHtmlContent: true,
                 scrollable: true,
-
                 items: {
                     docked: 'top',
                     xtype: 'titlebar',
                     title: 'Welcome to Sencha Touch 2'
                 },
-
                 html: [
                     "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
                     "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
@@ -32,7 +28,6 @@ Ext.define('TweetTheWeather.view.Main', {
             {
                 title: 'Get Started',
                 iconCls: 'action',
-
                 items: [
                     {
                         docked: 'top',
@@ -48,31 +43,29 @@ Ext.define('TweetTheWeather.view.Main', {
             }
         ]
     },
-    initialize: function(){
-        Geolocation.getCurrentCoordinates(function(err, coords) {
-                if(err && err.message === 'User denied Geolocation') {
-                    return alert('You have to allow Geolocation!')
+    initialize: function() {
+                    Geolocation.getCurrentCoordinates(function(err, coords) {
+                        if(err && err.message === 'User denied Geolocation') {
+                            return alert('You have to allow Geolocation!')
+                        }
+                        Ext.data.JsonP.request(
+                            { 
+                                url: 'http://api.worldweatheronline.com/free/v1/weather.ashx?q='+coords.latitude+'%2C'+coords.longitude+'&format=json&num_of_days=5&key=tsd3famkb4qeau8d9fx7gmjt',
+                                method: 'GET',
+                                success: function(result, request) {
+                                    console.log(result.data.current_condition[0].temp_C);
+                                }
+                            }
+                        );
+                        Ext.data.JsonP.request(
+                            { 
+                                url: 'http://api.worldweatheronline.com/free/v1/search.ashx?key=tsd3famkb4qeau8d9fx7gmjt&query='+coords.latitude+'%2C'+coords.longitude+'&num_of_results=1&format=json',
+                                method: 'GET',
+                                success: function(cityname, request) {
+                                    console.log(cityname.search_api.result[0].areaName[0].value);
+                                }
+                            }
+                        ); // just send the location name and the temperature to console according to geolocation
+                    });
                 }
-            
-            Ext.data.JsonP.request(
-                { 
-                    url: 'http://api.worldweatheronline.com/free/v1/weather.ashx?q='+coords.latitude+'%2C'+coords.longitude+'&format=json&num_of_days=5&key=tsd3famkb4qeau8d9fx7gmjt',
-                    method:'GET',
-                    success: function(result, request) {
-                        console.log(result.data.current_condition[0].temp_C);
-                    }
-                }
-            );
-            
-            Ext.data.JsonP.request(
-                { 
-                    url: 'http://api.worldweatheronline.com/free/v1/search.ashx?key=tsd3famkb4qeau8d9fx7gmjt&query='+coords.latitude+'%2C'+coords.longitude+'&num_of_results=1&format=json',
-                    method:'GET',
-                    success: function(cityname, request) {
-                        console.log(cityname.search_api.result[0].areaName[0].value);
-                    }
-                }
-            ); // just send the location name and the temperature to console according to geolocation
-        });
-    }
 });
