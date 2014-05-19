4a8f42458c2687c25e4212dd4d769327ade2b65a
andrIvash <andr.ivas12@gmail.com>
2014-05-19 17:57:24 +0300

    Update Place.js

--- app/store/Place.js
+++ app/store/Place.js
@@ -1,6 +1,5 @@
 Ext.define('TweetTheWeather.store.Place', {
     extend: 'Ext.data.Store',
-    
     config: {
         storeId: 'PlaceStore',    
         model: 'TweetTheWeather.model.Place',
@@ -9,8 +8,7 @@ Ext.define('TweetTheWeather.store.Place', {
                 type: 'jsonp',
                 url: 'http://api.worldweatheronline.com/free/v1/weather.ashx?q='+coords.latitude+'%2C'+coords.longitude+'&format=json&num_of_days=5&callback=callback&key=tsd3famkb4qeau8d9fx7gmjt',
                 callbackKey: 'callback',
-                callback: function(data){
-                    console.log(data.search_api.result[0].areaName[0].value);
+                callback: function(data) {
                 },
                 reader: {
                     type: 'json',
@@ -19,9 +17,9 @@ Ext.define('TweetTheWeather.store.Place', {
                 }
             }
     },
-    initialize: function(){
-        Geolocation.getCurrentCoordinates(function(err, coords) {
-            if(err && err.message === 'User denied Geolocation') {
+    initialize: function() {
+        Geolocation.getCurrentCoordinates(function(error, coords) {
+            if(error && error.message === 'User denied Geolocation') {
                 return alert('You have to allow Geolocation!');
             }
         });
